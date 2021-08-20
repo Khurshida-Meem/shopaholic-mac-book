2a1b228f6abb97309387fc6ed6756fd9f515bb12
Khurshida-Meem <meemrumi59@gmail.com>
2021-08-21 04:27:52 +0600

    final commit

--- app.js
+++ app.js
@@ -2,24 +2,46 @@
 // 512 SSD = 100
 // 1TB SSD = 180
 
-function addValue(extra, value) {
-    const initialCost = document.getElementById(extra + '-cost');
-    initialCost.innerText = value;
+// update price fields
+function addValue(value, price) {
+    const initialCost = document.getElementById(value + '-cost');
+    initialCost.innerText = price;
     totalSum();
 }
+// update custom price field
 function initialValue(value) {
     const cost = document.getElementById(value + '-cost');
     cost.innerText = 0;
     totalSum();
 }
-function totalSum() {
-    const sum = document.getElementById('total-cost');
-    const macCost = parseInt(document.getElementById('mac-cost').innerText);
-    const memoryCost = parseInt(document.getElementById('memory-cost').innerText);
-    const ssdCost = parseInt(document.getElementById('ssd-cost').innerText);
-    const deliveryCost = parseInt(document.getElementById('delivery-cost').innerText);
-    const totalSum = macCost + memoryCost + ssdCost + deliveryCost;
-    sum.innerText = totalSum;
+// take prices from id and make integer
+function getCost(value) {
+    return parseInt(document.getElementById(value + '-cost').innerText);
 }
 
+// total sum calculate
+function totalSum() {
+    const sum = document.getElementById('total-cost');
+    const finalCost = document.getElementById('final-cost');
+    const macCost = getCost('mac');
+    const memoryCost = getCost('memory');
+    const ssdCost = getCost('ssd');
+    const deliveryCost = getCost('delivery');
+    const totalSum = macCost + memoryCost + ssdCost + deliveryCost;
+    sum.innerText = totalSum;
+    finalCost.innerText = totalSum;
+}
 
+// coupon code er hisab nikash
+// stevekaku
+document.getElementById('apply-button').addEventListener('click', function () {
+    const couponField = document.getElementById('coupon-text');
+    const finalCost = document.getElementById('final-cost')
+    const couponText = couponField.value;
+    if (couponText == 'stevekaku') {
+        const totalCost = getCost('total');
+        const discountPrice = totalCost - (totalCost / 5);
+        finalCost.innerText = discountPrice;
+    }
+    couponField.value = '';
+});
